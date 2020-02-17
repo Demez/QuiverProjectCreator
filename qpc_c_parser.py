a38027ec30f71fb409fbbad8d81dad3f4c291f2a
Demez <demizexp@gmail.com>
2020-02-17 00:08:35 -0500

    [c_parser] fix encoding for c file parser and store parsed files

--- qpc_c_parser.py
+++ qpc_c_parser.py
@@ -4,12 +4,42 @@ import os.path
 include_pattern = re.compile(r"^[ \t]*#include[ \t]+[\"<]([a-zA-Z0-9\-_\.\/]+)[\">]")
 
 
+# speeds up this function by an insane amount
+INCLUDE_DICT = {
+    # "file_path": ["include list"]
+}
+
+
+def decode(data) -> str:
+    try:
+        return data.decode("UTF-8")
+    except UnicodeDecodeError:
+        try:
+            return data.decode("ASCII")
+        except UnicodeDecodeError:
+            return data.decode("ANSI")
+
+
 def get_includes(file_path: str) -> list:
+    try:
+        if file_path in INCLUDE_DICT:
+            return INCLUDE_DICT[file_path]
+        INCLUDE_DICT[file_path] = _get_includes(file_path)
+        return INCLUDE_DICT[file_path]
+    except UnicodeDecodeError as F:
+        print("UnicodeDecodeError: " + str(F) +
+              "\nFile: " + file_path)
+        return []
+
+
+def _get_includes(file_path: str) -> list:
     includes = []
-    with open(file_path, 'r') as f:
-        lines = f.read().split('\n')
-        for line in lines:
-            m = include_pattern.match(line)
-            if m and os.path.isfile(m.group(1)):
-                includes.append(m.group(1))
+    with open(file_path, 'rb') as f:
+        data = f.read()
+    text = decode(data)
+    lines = text.splitlines()
+    for line in lines:
+        m = include_pattern.match(line)
+        if m and os.path.isfile(m.group(1)):
+            includes.append(m.group(1))
     return includes
