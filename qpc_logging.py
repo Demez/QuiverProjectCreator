8e5086a04098a651005510efc69898ba5a2f945a
Demez <demizexp@gmail.com>
2020-05-13 23:16:36 -0400

    [feature] add qpc_logging.py, missed in previous commit

---
+++ qpc_logging.py
@@ -0,0 +1,87 @@
+import os
+import platform
+from qpc_args import args
+from enum import Enum
+
+
+_win32_legacy_con = False
+_win32_handle = None
+
+
+if os.name == "nt":
+    if platform.release().startswith("10"):
+        # hack to enter virtual terminal mode,
+        # could do it properly, but that's a lot of lines and this works just fine
+        import subprocess
+        subprocess.call('', shell=True)
+    else:
+        import ctypes
+        _win32_handle = ctypes.windll.kernel32.GetStdHandle(-11)
+        _win32_legacy_con = True
+
+
+class Color(Enum):
+    if _win32_legacy_con:
+        RED = "4"
+        GREEN = "2"
+        YELLOW = "6"
+        BLUE = "1"
+        MAGENTA = "5"
+        CYAN = "3"  # or 9
+    
+        DEFAULT = "7"
+    else:  # linux
+        RED = "\033[0;31m"
+        GREEN = "\033[0;32m"
+        YELLOW = "\033[0;33m"  # color 6?
+        BLUE = "\033[0;34m"
+        MAGENTA = "\033[0;35m"
+        CYAN = "\033[0;36m"
+        
+        DEFAULT = "\033[0m"
+
+
+class Severity(Enum):
+    VERBOSE = Color.GREEN
+    DEFAULT = Color.DEFAULT
+    WARNING = Color.YELLOW
+    ERROR = Color.RED
+    
+    
+WARNING_COUNT = 0
+
+
+def warning(*text):
+    if not args.hide_warnings:
+        _print_severity(Severity.WARNING, "\n          ", *text)
+    global WARNING_COUNT
+    WARNING_COUNT += 1
+
+
+def error(*text):
+    _print_severity(Severity.ERROR, "\n        ", *text)
+    quit(1)
+
+
+def verbose(*text):
+    if args.verbose:
+        _print_severity(Severity.VERBOSE, *text)
+
+
+def _print_severity(level: Severity, spacing: str, *text):
+    print_color(level.value, f"[{level.name}] {text[0]}{spacing}{spacing.join(text[1:])}\n")
+        
+        
+def win32_set_fore_color(color: int):
+    if not ctypes.windll.kernel32.SetConsoleTextAttribute(_win32_handle, color):
+        print(f"[ERROR] WIN32 Changing Colors Failed, Error Code: {str(ctypes.GetLastError())},"
+              f" color: {color}, handle: {str(_win32_handle)}")
+    
+    
+def print_color(color: Color, *text):
+    if _win32_legacy_con:
+        win32_set_fore_color(int(color.value))
+        print("".join(text))
+        win32_set_fore_color(int(Color.DEFAULT.value))
+    else:
+        print(color.value + "".join(text) + Color.DEFAULT.value)
