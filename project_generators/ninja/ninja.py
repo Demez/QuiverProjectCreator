de168d46b4e6c29c4f2f8cb88a29e19e502bbd15
swissChili <swisschili@fastmail.com>
2020-05-20 13:27:09 -0700

    Fix ninja script generating overlapping build rules, make compiler project-specific

--- project_generators/ninja/ninja.py
+++ project_generators/ninja/ninja.py
@@ -35,7 +35,8 @@ class NinjaGenerator(BaseProjectGenerator):
         create_directory("build_ninja")
         for label, commands_list in self.commands_list.items():
             print_color(Color.CYAN, "Writing: " + f"build_ninja/{label}.ninja")
-            script = '\n\n'.join(commands_list)
+            script = self.gen_rules()
+            script += '\n\n'.join(commands_list)
             with open(f"build_ninja/{label}.ninja", "w") as file_io:
                 file_io.write(script)
     
@@ -44,7 +45,10 @@ class NinjaGenerator(BaseProjectGenerator):
         if not project_passes:
             return
 
+        proj_name = project.file_name.replace('.', '_')
+
         print_color(Color.CYAN, "Adding to Ninja: " + project.file_name)
+
         
         for proj_pass in project_passes:
             conf = proj_pass.config
@@ -58,22 +62,17 @@ class NinjaGenerator(BaseProjectGenerator):
             compiler = get_compiler(proj_pass.config.general.compiler,
                                     proj_pass.config.general.language)
 
-            self.commands_list[label].append(self.gen_header(conf, project, compiler))
-                
+            self.commands_list[label].append(self.gen_header(conf, project, compiler, proj_name))
+                   
             for file in proj_pass.source_files:
                 if file not in self.all_files[label]:
                     self.all_files[label].add(file)
-                    self.commands_list[label].append(self.handle_file(file, proj_pass))
+                    self.commands_list[label].append(self.handle_file(file, proj_pass, proj_name))
 
             self.commands_list[label].append(self.handle_target(conf, proj_pass.source_files))
 
-    def gen_header(self, conf, project, compiler):
-        outname = conf.general.out_name if conf.general.out_name else project.container.file_name
-        return f"""#!/usr/bin/env ninja -f
-# variables
-srcdir = {os.getcwd()}
-out_file = {outname}
-compiler = {compiler}
+    def gen_rules(self) -> str:
+        return f"""
 
 # rules
 rule cc
@@ -89,6 +88,15 @@ rule so
     command = $compiler -o $out $in -fPIC -shared $cflags
 """
 
+    def gen_header(self, conf, project, compiler, proj_name: str):
+        outname = conf.general.out_name if conf.general.out_name else project.container.file_name
+        return f"""#!/usr/bin/env ninja -f
+# variables
+{proj_name}_srcdir = {os.getcwd()}
+out_file = {outname}
+{proj_name}_compiler = {compiler}
+"""
+
     def handle_target(self, conf, source_files) -> str:
         target_name = conf.linker.output_file if conf.linker.output_file else "$out_file"
         objs = ' '.join([ a + '.o'  for a in source_files ])
@@ -102,13 +110,14 @@ rule so
 
     # Build definition for file
     # TODO: only supports objects rn, add so and exe support
-    def handle_file(self, file: str, project: ProjectPass) -> str:
+    def handle_file(self, file: str, project: ProjectPass, proj_name: str) -> str:
         print(os.getcwd(), project)
-        cmd = f"build {file}.o: cc $srcdir/{file}\n"
+        cmd = f"build {file}.o: cc ${proj_name}_srcdir/{file}\n"
         
         defs = " ".join(self.cmd_gen.convert_defines(project.config.compiler.preprocessor_definitions))
         includes = " ".join(self.cmd_gen.convert_includes(project.config.general.include_directories))
         cflags = " ".join(project.config.compiler.options)
         cmd += f"    cflags = {cflags} {includes} {defs}\n"
+        cmd += f"    compiler = {proj_name}_compiler\n"
 
         return cmd
